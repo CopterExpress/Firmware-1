0c3a256c19a53c6049bef17121453a4985258caf
Oleg Kalachev <okalachev@gmail.com>
2021-01-26 14:32:38 +0300

    Revert "Tools: update ubuntu setup script, Python requirements"
    This reverts commit b94586dc12ae98677bf130c9c77d875efc66555d.

--- Tools/setup/ubuntu.sh
+++ Tools/setup/ubuntu.sh
@@ -65,8 +65,6 @@ elif [[ "${UBUNTU_RELEASE}" == "16.04" ]]; then
 	echo "Ubuntu 16.04"
 elif [[ "${UBUNTU_RELEASE}" == "18.04" ]]; then
 	echo "Ubuntu 18.04"
-elif [[ "${UBUNTU_RELEASE}" == "20.04" ]]; then
-	echo "Ubuntu 20.04"
 fi
 
 
@@ -81,8 +79,11 @@ sudo apt-get -yy --quiet --no-install-recommends install \
 	astyle \
 	build-essential \
 	ccache \
+	clang \
+	clang-tidy \
 	cmake \
 	cppcheck \
+	doxygen \
 	file \
 	g++ \
 	gcc \
@@ -91,14 +92,16 @@ sudo apt-get -yy --quiet --no-install-recommends install \
 	lcov \
 	make \
 	ninja-build \
-	python3 \
-	python3-dev \
 	python3-pip \
+	python3-pygments \
 	python3-setuptools \
-	python3-wheel \
+	python-pip \
+	python-dev \
 	rsync \
 	shellcheck \
 	unzip \
+	wget \
+	xsltproc \
 	zip \
 	;
 
@@ -113,7 +116,15 @@ fi
 echo
 echo "Installing PX4 Python3 dependencies"
 sudo python3 -m pip install --upgrade pip setuptools wheel
-python3 -m pip install --user -r ${DIR}/requirements.txt
+sudo python3 -m pip install -r ${DIR}/requirements.txt
+
+
+# Python2 dependencies
+echo
+echo "Installing PX4 Python2 dependencies"
+sudo python2 -m pip install --upgrade pip setuptools wheel
+sudo python2 -m pip install -r ${DIR}/requirements.txt
+
 
 # NuttX toolchain (arm-none-eabi-gcc)
 if [[ $INSTALL_NUTTX == "true" ]]; then
@@ -126,14 +137,12 @@ if [[ $INSTALL_NUTTX == "true" ]]; then
 		automake \
 		bison \
 		bzip2 \
-		file \
 		flex \
 		gdb-multiarch \
 		gperf \
 		libncurses-dev \
 		libtool \
 		pkg-config \
-		screen \
 		vim-common \
 		;
 
@@ -144,27 +153,24 @@ if [[ $INSTALL_NUTTX == "true" ]]; then
 	sudo apt-get remove modemmanager -y
 
 	# arm-none-eabi-gcc
-	NUTTX_GCC_VERSION="9-2020-q2-update"
-	NUTTX_GCC_VERSION_SHORT="9-2020q2"
+	NUTTX_GCC_VERSION="7-2017-q4-major"
 
-	source $HOME/.profile # load changed path for the case the script is reran before relogin
-	if [ $(which arm-none-eabi-gcc) ]; then
-		GCC_VER_STR=$(arm-none-eabi-gcc --version)
-		GCC_FOUND_VER=$(echo $GCC_VER_STR | grep -c "${NUTTX_GCC_VERSION}")
-	fi
+	GCC_VER_STR=$(arm-none-eabi-gcc --version)
+	STATUSRETVAL=$(echo $GCC_VER_STR | grep -c "${NUTTX_GCC_VERSION}")
 
-	if [[ "$GCC_FOUND_VER" == "1" ]]; then
+	if [ $STATUSRETVAL -eq "1" ]; then
 		echo "arm-none-eabi-gcc-${NUTTX_GCC_VERSION} found, skipping installation"
 
 	else
 		echo "Installing arm-none-eabi-gcc-${NUTTX_GCC_VERSION}";
-		wget -O /tmp/gcc-arm-none-eabi-${NUTTX_GCC_VERSION}-linux.tar.bz2 https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/${NUTTX_GCC_VERSION_SHORT}/gcc-arm-none-eabi-${NUTTX_GCC_VERSION}-x86_64-linux.tar.bz2 && \
+		wget -O /tmp/gcc-arm-none-eabi-${NUTTX_GCC_VERSION}-linux.tar.bz2 https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/7-2017q4/gcc-arm-none-eabi-${NUTTX_GCC_VERSION}-linux.tar.bz2 && \
 			sudo tar -jxf /tmp/gcc-arm-none-eabi-${NUTTX_GCC_VERSION}-linux.tar.bz2 -C /opt/;
 
 		# add arm-none-eabi-gcc to user's PATH
 		exportline="export PATH=/opt/gcc-arm-none-eabi-${NUTTX_GCC_VERSION}/bin:\$PATH"
 
-		if grep -Fxq "$exportline" $HOME/.profile; then
+		if grep -Fxq "$exportline" $HOME/.profile;
+		then
 			echo "${NUTTX_GCC_VERSION} path already set.";
 		else
 			echo $exportline >> $HOME/.profile;
@@ -179,46 +185,25 @@ if [[ $INSTALL_SIM == "true" ]]; then
 	echo
 	echo "Installing PX4 simulation dependencies"
 
-	# General simulation dependencies
-	sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install \
-		bc \
-		;
-
-	if [[ "${UBUNTU_RELEASE}" == "18.04" ]]; then
-		java_version=11
-		gazebo_version=9
-	elif [[ "${UBUNTU_RELEASE}" == "20.04" ]]; then
-		java_version=14
-		gazebo_version=11
-	else
-		java_version=14
-		gazebo_version=11
-	fi
-	# Java (jmavsim or fastrtps)
-	sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install \
+	# java (jmavsim or fastrtps)
+	sudo apt-get -yy --quiet --no-install-recommends install \
 		ant \
-		openjdk-$java_version-jre \
-		openjdk-$java_version-jdk \
-		libvecmath-java \
+		openjdk-8-jre \
+		openjdk-8-jdk \
 		;
-
-	# Set Java 11/14 as default
-	sudo update-alternatives --set java $(update-alternatives --list java | grep "java-$java_version")
 
 	# Gazebo
 	sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 	wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
-	sudo apt-get update -y --quiet
-	sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install \
-		dmidecode \
-		gazebo$gazebo_version \
+	sudo apt-get update -yy --quiet
+	sudo apt-get -yy --quiet --no-install-recommends install \
+		gazebo9 \
 		gstreamer1.0-plugins-bad \
 		gstreamer1.0-plugins-base \
 		gstreamer1.0-plugins-good \
 		gstreamer1.0-plugins-ugly \
-		gstreamer1.0-libav \
 		libeigen3-dev \
-		libgazebo$gazebo_version-dev \
+		libgazebo9-dev \
 		libgstreamer-plugins-base1.0-dev \
 		libimage-exiftool-perl \
 		libopencv-dev \
